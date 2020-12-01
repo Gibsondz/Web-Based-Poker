fcf7887d84c6b99af0af4b01153ac61d82dc2cc3
mglyngberg <mglyngberg2@gmail.com>
2020-11-30 20:13:14 -0700

    Implement all admin page ui items::

--- Client/src/components/browse/AdminPage.vue
+++ Client/src/components/browse/AdminPage.vue
@@ -1,8 +1,25 @@
 <template>
     <v-container class="container">
-        <h1>Hello</h1>
-        <el-collapse >
-        </el-collapse>
+        <h1>Manage Users</h1>
+
+        <div class="user" v-for="(user, i) in users" v-bind:key="user.username">
+            <div>
+                <b-button v-b-toggle="'collapse-' + i" variant="primary">{{ user.username }}</b-button>
+                <p> {{ user.userType }} </p>
+            </div>
+            <b-collapse :id="'collapse-' + i" class="mt-2">
+                <b-card>
+                    <div>
+                        <p> Change Password </p>
+                        <v-text-field v-model="pwChanges[i]" label="Username"></v-text-field>
+                        <v-btn primary small block @click="changePassword(user.username, i)">Change Password</v-btn>
+                        <v-btn primary small block @click="makeAdmin(user.username)">Admin</v-btn>
+                        <v-btn primary small block @click="makeStandard(user.username)">Standard</v-btn>
+                        <v-btn primary large block @click="removeUser(user.username)">Remove User</v-btn>
+                    </div>
+                </b-card>
+            </b-collapse>
+        </div>        
     </v-container>
 </template>
 <script>
@@ -24,28 +41,32 @@ export default {
                 {username: "coolguy", userType:"standard"},
                 {username: "eaeeee", userType:"standard"}
             ],
-            // isHost:true,
-            
-        //     host:'bob',
-        //     gameData:{
-        //         startingStack:3000,
-        //         blindTimer:'15 minutes',
-        //         startingBlinds:'15/30',
-        //     },
-        //     players: [{
-        //         username: "robby"
-        //     },
-        //     {
-        //         username: "dilbert"
-        //     },
-        //     {
-        //         username: "bobby"
-        //     }],
+
+            pwChanges: {},
         }
     },
     async created() {
     },    
     methods:{
+        async changePassword(username, i) {
+            console.log(username);
+            console.log(this.pwChanges[i]);
+            if(confirm("Are you sure?")){
+
+            }
+        },
+        async makeAdmin(username) {
+            console.log(username);
+        },
+        async makeStandard(username) {
+            console.log(username);
+        },
+        async removeUser(username) {
+            console.log(username);
+            if(confirm("Are you sure?")){
+
+            }
+        },
     }
 }
 </script>
